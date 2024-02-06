21d75b3be94e11d10c431d75fefcaf21dc2932ca
cjayers <ayerscj@mail.uc.edu>
2024-02-05 21:48:09 -0500

    extra credit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        //Declare variable
+
+        final int m = 25;
+
+        final int mm = 92;
+
+        if (mm % 2 == 0)
+
+           System.out.println("The number " + mm + " is even.");
+        else
+
+            System.out.println("The number " + mm + " is odd.");
+        if (m % 2 == 0)
+
+            System.out.println("Then number " + m + " is even");
+        else
+
+            System.out.println("The number " + m + " is odd.");
+
+
+    }
+
+
+
+
+}
